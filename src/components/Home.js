cd20f2b64897f8bfc896fe0cc878e9ac75570f5e
hunferno <kone.rene@hotmail.fr>
2021-11-06 15:26:36 +0100

    Home page fini avec les props...debut de Header page

---
+++ src/components/Home.js
@@ -0,0 +1,64 @@
+import React from "react";
+import styled from "styled-components";
+import Section from "./Section";
+
+function Home() {
+  return (
+    <Container>
+      <Section
+        title="Model S"
+        description="Order Online for Touchless Delivery"
+        leftBtnText="Custom Order"
+        rightBtnText="Existing Inventory"
+        backgroundImg="model-s.jpg"
+      />
+      <Section
+        title="Model Y"
+        description="Order Online for Touchless Delivery"
+        leftBtnText="Custom Order"
+        rightBtnText="Existing Inventory"
+        backgroundImg="model-y.jpg"
+      />
+      <Section
+        title="Model 3"
+        description="Order Online for Touchless Delivery"
+        leftBtnText="Custom Order"
+        rightBtnText="Existing Inventory"
+        backgroundImg="model-3.jpg"
+      />
+      <Section
+        title="Model X"
+        description="Order Online for Touchless Delivery"
+        leftBtnText="Custom Order"
+        rightBtnText="Existing Inventory"
+        backgroundImg="model-x.jpg"
+      />
+      <Section
+        title="Lower Cost Solar Panels In Amercia"
+        description="Money-back guarantee"
+        leftBtnText="Order now"
+        rightBtnText="Learn more"
+        backgroundImg="solar-panel.jpg"
+      />
+      <Section
+        title="Solar for New Roofs"
+        description="Solar Roof Costs Less Than a New Panel"
+        leftBtnText="Order now"
+        rightBtnText="Learn more"
+        backgroundImg="solar-roof.jpg"
+      />
+      <Section
+        title="Accessories"
+        description=""
+        leftBtnText="Shop now"
+        backgroundImg="accessories.jpg"
+      />
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  height: 100vh;
+`;
+
+export default Home;

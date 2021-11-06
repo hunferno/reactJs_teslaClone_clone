51bbedb60fb018b9a14894f1f5e1286ce69020d8
hunferno <kone.rene@hotmail.fr>
2021-11-06 23:09:53 +0100

    redux Slice des cars fait...

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,117 @@
-import React from "react";
+import React, { useState } from "react";
+import styled from "styled-components";
+import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
+import { selectCars } from "../features/car/carSlice";
+import { useSelector } from "react-redux";
 
 function Header() {
-  return <div>header</div>;
+  const [burgerStatus, setBurgerStatus] = useState(false);
+  const cars = useSelector(selectCars);
+  console.log(cars);
+  return (
+    <Container>
+      <a href="#">
+        <img src="/images/logo.svg" alt="" />
+      </a>
+      <Menu>
+        {cars &&
+          cars.map((car, index) => (
+            <li>
+              <a key={index} href="">
+                {car}
+              </a>
+            </li>
+          ))}
+      </Menu>
+      <MenuRight>
+        <a href="#">Shop</a>
+        <a href="#">Tesla Account</a>
+        <CustomMenu onClick={() => setBurgerStatus(true)} />
+      </MenuRight>
+      <BurgerNav show={burgerStatus}>
+        <CustomClose onClick={() => setBurgerStatus(false)} />
+        <li>
+          <a href="">Existing Inventory</a>
+        </li>
+        <li>
+          <a href="">Used Inventory</a>
+        </li>
+        <li>
+          <a href="">Trade-in</a>
+        </li>
+        <li>
+          <a href="">Cybertruck</a>
+        </li>
+        <li>
+          <a href="">Roadster</a>
+        </li>
+      </BurgerNav>
+    </Container>
+  );
 }
 
 export default Header;
+
+const Container = styled.div`
+  width: 100vw;
+  min-height: 60px;
+  position: fixed;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 20px;
+  z-index: 10;
+`;
+const Menu = styled.div`
+  display: flex;
+  align-items: center;
+  a {
+    font-weight: 600;
+    text-transform: uppercase;
+    padding: 0 10px;
+  }
+  @media (max-width: 768px) {
+    display: none;
+  }
+`;
+const MenuRight = styled.div`
+  display: flex;
+  align-items: center;
+  a {
+    font-weight: 600;
+    text-transform: uppercase;
+    margin-right: 10px;
+  }
+`;
+
+const CustomMenu = styled(MenuIcon)`
+  cursor: pointer;
+`;
+
+const BurgerNav = styled.div`
+  background: white;
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  width: 300px;
+  list-style: none;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  text-align: start;
+  transform: ${(props) => (props.show ? "translateX(0)" : "translateX(100%)")};
+  transition: transform 0.2s ease-in;
+  li {
+    padding: 15px 0;
+    border-bottom: 1px solid rgba(0, 0, 0, 0.2);
+  }
+  a {
+    font-weight: 600;
+  }
+`;
+const CustomClose = styled(CloseIcon)`
+  cursor: pointer;
+  align-self: flex-end;
+`;
